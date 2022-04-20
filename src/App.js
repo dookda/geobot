c43812f9c59b7b401dccd02c522a59e9803844f0
dookda <sakda.homhuan@gmail.com>
2022-04-20 10:32:27 +0700

    update new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css';
+import Nav from './component/Nav';
+
+function App() {
+  return (
+    <div >
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
