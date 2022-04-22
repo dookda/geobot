9726b0927a1fd75a7c36dd388936b8d23bdf0d17
dookda <sakda.homhuan@gmail.com>
2022-04-22 10:26:53 +0700

    update line login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect, useState } from 'react';
 import './App.css';
 import Nav from './component/Nav';
 
+const liff = window.liff;
+
 function App() {
+  const [displayName, setDisplayName] = useState('')
+  const [userId, setUserId] = useState('')
+  const [pictureUrl, setPictureUrl] = useState('')
+
+  const initLine = async () => {
+    await liff.init({ liffId: '1657043590-Qe6npdDk' }).catch(err => { throw err })
+    if (liff.isLoggedIn()) {
+      let getProfile = await liff.getProfile();
+      setDisplayName(getProfile.displayName);
+      setUserId(getProfile.userId);
+      setPictureUrl(getProfile.pictureUrl);
+    } else {
+      liff.login();
+    }
+  }
+
+  useEffect(() => {
+    initLine();
+    console.log("hello");
+  }, [])
+
   return (
     <div >
       <Nav />
+      {displayName}
+      {userId}
+      {pictureUrl}
     </div>
   );
 }
